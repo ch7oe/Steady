dafd4b8084a0ba040c39f1df06943d90fdae7d1f
ch7oe <chloenixon723@gmail.com>
2025-05-26 10:23:58 -0400

    moved swallow_difficulty to User class and removed Setting class

--- model.py
+++ model.py
@@ -14,6 +14,7 @@ class User(db.Model):
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     signup_date = db.Column(db.DateTime, nullable=False)
+    swallow_difficulty = db.Column(db.boolean)
 
     setting = db.relationship("Setting", back_populates="user")
     diet_restrictions = db.relationship("DietaryRestriction", back_populates="user")
@@ -27,21 +28,6 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User user_id={self.user_id} fname={self.fname} lname={self.lname}>"
-    
-
-class Setting(db.Model):
-    """A user's setting."""
-
-    __tablename__ = "settings"
-
-    setting_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
-    swallow_difficulty = db.Column(db.boolean)
-
-    user = db.relationship("User", back_populates="setting")
-
-    def __repr__(self):
-        return f"<Setting setting_id={self.setting_id}>"
     
 
 class DietaryRestriction(db.Model):
@@ -258,7 +244,6 @@ class MealPlan(db.Model):
     user = db.relationship("User", back_populates="meal_plans")
     meal_plan_recipes = db.relationship("MealPlanRecipe", back_populates="meal_plan")
 
-
     def __repr__(self):
         return f"<MealPlan meal_plan_id={self.meal_plan_id} meal_plan_date={self.meal_plan_date}>"
     
@@ -276,12 +261,23 @@ class MealPlanRecipe(db.Model):
     recipe = db.relationship("Recipe", back_populates="meal_plan_recipes")
     meal_plan = db.relationship("MealPlan", back_populates="meal_plan_recipes")
 
-
     def __repr__(self):
         return f"<MealPlanRecipe meal_plan_id={self.meal_plan_id} recipe_id={self.recipe_id} meal_type={self.meal_type}>"
     
 
 
+def connect_to_db(flask_app, db_uri="postgresql:///steady", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected! :)")
 
 
-
+if __name__ == "__main__":
+    from server import app
+    connect_to_db(app)
+    app.app_context().push()
