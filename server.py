9f369b45a2cff091445a0a57f35247965a063ba4
ch7oe <chloenixon723@gmail.com>
2025-06-15 03:01:25 -0400

    created /register for GET/POST request to create user

--- server.py
+++ server.py
@@ -26,27 +26,53 @@ def register():
         fname = request.form.get("fname")
         lname = request.form.get("lname")
         email = request.form.get("email")
-        password = request.form.get("password")
+        raw_password = request.form.get("password")
         swallow_difficulty = request.form.get("swallow-diff")
         weight = request.form.get("weight")
 
         existing_user = crud.get_user_by_email(email)
+        
         if existing_user: # check is user already exists
             flash("An account with this email already exists. Please login.")
             return redirect("/login")
         else:
-            new_user = crud.create_user(
+            new_user = crud.create_user( # create user in database with hashed password
                 fname=fname,
                 lname=lname,
                 email=email,
-                password=password,
+                raw_password=raw_password,
                 swallow_difficulty=swallow_difficulty,
                 weight=weight
             )
+            db.session.add(new_user)
+            db.session.commit()
 
+            # login user automatically after sign up
+            session["user_id"] = new_user.user_id
 
+            flash(f"Welcome, {new_user.fname}! Account created successfully.")
+            return redirect("/dashboard")
 
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login_user():
+    """Login a user."""
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        raw_password = request.form.get("password") 
+
+        user = crud.get_user_by_email(email)
+
+        if not user: # if no user with given email exists
+
+
+@app.route("/dashboard")
+
+
+
 
 
 
